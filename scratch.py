cbe7d5a42699a824e7d1b61a43ad8bcaf713d911
Caroline Hughes <carolinehughes1717@gmail.com>
2022-12-08 12:11:53 -0500

    refactoring and some more logic around log reconciliation

---
+++ scratch.py
@@ -0,0 +1,25 @@
+# very first ae from very first leader looks like
+
+# Sent message: {
+# 'src': '0000', 
+# 'dst': 'FFFF', 
+# 'leader': '0000', 
+# 'type': 'ae', 
+# 'term': 1, 
+# 'prev_log_i': -1, 
+# 'prev_log_term': -1, 
+# 'entries': [[1, '0Q6YGKG39WOHGR54XTRB5KGE4MJ7UJMZ', '2edcc4222c2bbb9ffab851a4cb30fc9c']], 
+# 'commit': -1}
+
+# next ae from same leader
+
+# Sent message: {
+#   'src': '0000', 
+# 'dst': 'FFFF', 
+# 'leader': '0000', 
+# 'type': 'ae', 
+# 'term': 1, 
+# 'prev_log_i': 0, 
+# 'prev_log_term': 1, 
+# 'entries': [[1, '8S6FCEROFMEE8D5FN3N7Z54YRTVP7RE2', '1b8a5f0d917f5e1a41833bc3f0f0879f']], 
+# 'commit': 0}
